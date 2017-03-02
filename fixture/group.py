a6275920ffe8991016159bcf9aad387566568b12
holi87 <holi87@gmail.com>
2017-03-02 21:58:43 +0100

    group cache and hash

--- fixture/group.py
+++ fixture/group.py
@@ -22,6 +22,7 @@ class GroupHelper(fixture.basic.BasicHelper):
         self.fill_group_form(group)
         wd.find_element_by_name("submit").click()
         self.open_groups_page()  # as return to groups page
+        self.group_cache = None
 
     def delete_first_group(self):
         wd = self.app.wd
@@ -29,6 +30,7 @@ class GroupHelper(fixture.basic.BasicHelper):
         self.select_first_group()
         wd.find_element_by_name("delete").click()
         self.open_groups_page()
+        self.group_cache = None
 
     def select_first_group(self):
         wd = self.app.wd
@@ -45,6 +47,7 @@ class GroupHelper(fixture.basic.BasicHelper):
         # submit group creation
         wd.find_element_by_name("update").click()
         self.open_groups_page()  # as return to groups page
+        self.group_cache = None
 
     def fill_group_form(self, group):
         wd = self.app.wd
@@ -57,12 +60,15 @@ class GroupHelper(fixture.basic.BasicHelper):
         self.open_groups_page()
         return len(wd.find_elements_by_name("selected[]"))
 
+    group_cache = None
+
     def get_group_list(self):
-        wd = self.app.wd
-        self.open_groups_page()
-        groups = []
-        for element in wd.find_elements_by_css_selector("span.group"):
-            text = element.text
-            group_id = element.find_element_by_name("selected[]").get_attribute("value")
-            groups.append(Group(name=text, group_id=group_id))
-        return groups
+        if self.group_cache is None:
+            wd = self.app.wd
+            self.open_groups_page()
+            self.group_cache = []
+            for element in wd.find_elements_by_css_selector("span.group"):
+                text = element.text
+                group_id = element.find_element_by_name("selected[]").get_attribute("value")
+                self.group_cache.append(Group(name=text, group_id=group_id))
+        return list(self.group_cache)

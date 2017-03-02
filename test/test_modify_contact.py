ef5b197afd92e43803e7e3f8175c6aa35fe23912
holi87 <holi87@gmail.com>
2017-03-02 21:39:41 +0100

    task 11.

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -3,19 +3,31 @@ __author__ = "Grzegorz Holak"
 
 
 def test_modify_first_contact(app):
+    old_contacts = app.contact.get_contact_list()
+
+    contact = Contact(first_name="jakies losowe", last_name="nazwisko siakie")
+    contact.contact_id = old_contacts[0].contact_id
+
     if app.contact.count() == 0:
-        app.contact.create(Contact(first_name="jakies losowe", last_name="nazwisko siakie", email="email@maryna.xt"
-                                   , mobile_phone="1241241"))
-    app.contact.modify_first_contact_without_photo(Contact(first_name="noweImie", last_name="noweNazwisko"
-                                                           , mobile_phone="99999", email="newemail@email.pl"
-                                                           , birthday_day=27, birthday_month=11, birthday_year="1988"
-                                                           , address="Gdzieś w Mińsku Mazowieckim"))
+        app.contact.create(Contact(first_name="imieDodane", last_name="NazwiskoDodane"))
+    app.contact.modify_first_contact_without_photo(contact)
+    new_contacts = app.contact.get_contact_list()
+
+    assert len(old_contacts) == len(new_contacts)
+    old_contacts[0] = contact
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
 
 def test_modify_first_name(app):
+    old_contacts = app.contact.get_contact_list()
+    contact = Contact(first_name="jakies losowe", last_name=None)
+    contact.contact_id = old_contacts[0].contact_id
+    contact.last_name = old_contacts[0].last_name  # because in object contact last_name is None, same like for id
+
     if app.contact.count() == 0:
-        app.contact.create(Contact(first_name="jakies losowe", last_name="nazwisko siakie", email="email@maryna.xt"
-                                   , mobile_phone="1241241"))
-    # need to make none only for these, as in model.class only these four are required - keep it simple :)
-    app.contact.modify_first_contact_without_photo(Contact(first_name="noweImie", last_name=None
-                                                           , mobile_phone=None, email=None))
+        app.contact.create(Contact(first_name="imieDodane", last_name="NazwiskoDodane"))
+    app.contact.modify_first_contact_without_photo(contact)
+    new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) == len(new_contacts)
+    old_contacts[0] = contact
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)

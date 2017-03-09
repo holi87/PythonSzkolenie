daa1f1d9dad46679813b9f05f51d7b821fef1f55
holi87 <holi87@gmail.com>
2017-03-09 11:05:54 +0100

    get contact info from edit page

--- fixture/contact.py
+++ fixture/contact.py
@@ -131,7 +131,6 @@ class ContactHelper(fixture.basic.BasicHelper):
                 first_name = cells[2].text
                 contact_id = cells[0].find_element_by_tag_name("input").get_attribute("value")
                 self.contact_cache.append(Contact(first_name=first_name, last_name=last_name, contact_id=contact_id))
-
         return list(self.contact_cache)
 
     def open_contact_to_edit_by_index(self, index):
@@ -147,3 +146,16 @@ class ContactHelper(fixture.basic.BasicHelper):
         row = wd.find_elements_by_name("entry")[index]
         cell = row.find_elements_by_tag_name("td")[6]
         cell.find_element_by_tag_name("a").click()
+
+    def get_contact_info_from_edit_page(self, index):
+        wd = self.app.wd
+        self.open_contact_to_edit_by_index(index)
+        homephone = wd.find_element_by_name("home").get_attribute("value")
+        mobilephone = wd.find_element_by_name("mobile").get_attribute("value")
+        workphone = wd.find_element_by_name("work").get_attribute("value")
+        secondaryphone = wd.find_element_by_name("phone2").get_attribute("value")
+        firstname = wd.find_element_by_name("firstname").get_attribute("value")
+        lastname = wd.find_element_by_name("lastname").get_attribute("value")
+        contact_id = wd.find_element_by_name("id").get_attribute("value")
+        return Contact(first_name=firstname, last_name=lastname, contact_id=contact_id, mobile_phone=mobilephone
+                       , home_phone=homephone, work_phone=workphone, home_phone2=secondaryphone)

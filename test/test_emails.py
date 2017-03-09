1549f8d8e19a77c9acb925d8d2fc87f8328e5b0f
holi87 <holi87@gmail.com>
2017-03-09 14:25:29 +0100

    task 14 working on

--- test/test_emails.py
+++ test/test_emails.py
@@ -10,5 +10,5 @@ def test_names_on_home_page(app):
     index = randrange(len(app.contact.get_contact_list()))
     contact_from_home_page = app.contact.get_contact_list()[index]
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
-    assert contact_from_home_page.all_phones_from_home_page == app.\
-        contact.merge_phones_like_on_home_page(contact_from_edit_page)
+    assert contact_from_home_page.all_emails_from_home_page == app.\
+        contact.merge_emails_like_on_home_page(contact_from_edit_page)

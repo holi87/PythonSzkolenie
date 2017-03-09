50580c8137b4f7b1dd97c8b90db238fe5f61732a
holi87 <holi87@gmail.com>
2017-03-09 14:54:09 +0100

    task 14 done

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,25 +1,20 @@
 # -*- coding: utf-8 -*-
-
+import pytest
 
 from model.group import Group
 
+testdata = [
+    Group(name="losowanazwa", header="jakisheader", footer="jakiscomment")
+    Group(name="", header="", footer="")
+]
 
-def test_add_group(app):
+@pytest.mark.parametrize("group", testdata)
+def test_add_group(app, group):
     old_groups = app.group.get_group_list()
-    group = Group(name="losowanazwa", header="jakisheader", footer="jakiscomment")
     app.group.create(group)
     assert len(old_groups) + 1 == app.group.count()
     new_groups = app.group.get_group_list()
     old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
-# commented because no need to use second test for now.
-#
-# def test_add_empty_group(app):
-#     group = Group(name="", header="", footer="")
-#     old_groups = app.group.get_group_list()
-#     app.group.create(group)
-#     new_groups = app.group.get_group_list()
-#     assert len(old_groups) + 1 == len(new_groups)
-#     old_groups.append(group)
-#     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+

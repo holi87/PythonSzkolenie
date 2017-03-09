b380893de6b5812ea387d364111eb7487879e0aa
holi87 <holi87@gmail.com>
2017-03-09 14:26:16 +0100

    test for emails done

--- test/test_emails.py
+++ test/test_emails.py
@@ -6,7 +6,8 @@ __author__ = "Grzegorz Holak"
 def test_names_on_home_page(app):
     # when there is no contact - make one for test
     if app.contact.count() == 0:
-        app.contact.create(Contact(first_name="jakies losowe", last_name="nazwisko siakie"))  # need only names here
+        app.contact.create(Contact(first_name="jakies losowe", last_name="nazwisko siakie", email="tata@tat.kl"
+                                   , email2="trutu@kl.tw"))
     index = randrange(len(app.contact.get_contact_list()))
     contact_from_home_page = app.contact.get_contact_list()[index]
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)

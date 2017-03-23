7f6437331eeef155239fb706518bc342f7dc3d30
holi87 <holi87@gmail.com>
2017-03-23 21:07:07 +0100

    compare times to get group list from UI(x1) and from DB (x1000)

--- test/test_db_matches_ui.py
+++ test/test_db_matches_ui.py
@@ -1,11 +1,14 @@
 from model.group import Group
+from timeit import timeit
 __author__ = "Grzegorz Holak"
 
 
 def test_group_list(app, db):
-    ui_list = app.group.get_group_list()
+    # ui_list = app.group.get_group_list()
+    print(timeit(lambda: app.group.get_group_list(), number=1))
 
     def clean(group):
         return Group(group_id=group.group_id, name=group.name.strip())
-    db_list = map(clean, db.get_group_list())
-    assert sorted(ui_list, key=Group.id_or_max) == sorted(db_list, key=Group.id_or_max)
+    # db_list = map(clean, db.get_group_list())
+    print(timeit(lambda: map(clean, db.get_group_list()), number=1000))
+    assert False  #  sorted(ui_list, key=Group.id_or_max) == sorted(db_list, key=Group.id_or_max)

7dfca36cbd2ea0d80e0693b1b6fa088e53e103ff
holi87 <holi87@gmail.com>
2017-03-23 21:10:36 +0100

    back to normal test

--- test/test_db_matches_ui.py
+++ test/test_db_matches_ui.py
@@ -4,11 +4,11 @@ __author__ = "Grzegorz Holak"
 
 
 def test_group_list(app, db):
-    # ui_list = app.group.get_group_list()
-    print(timeit(lambda: app.group.get_group_list(), number=1))
+    ui_list = app.group.get_group_list()
+    # print(timeit(lambda: app.group.get_group_list(), number=1))
 
     def clean(group):
         return Group(group_id=group.group_id, name=group.name.strip())
-    # db_list = map(clean, db.get_group_list())
-    print(timeit(lambda: map(clean, db.get_group_list()), number=1000))
-    assert False  #  sorted(ui_list, key=Group.id_or_max) == sorted(db_list, key=Group.id_or_max)
+    db_list = map(clean, db.get_group_list())
+    # print(timeit(lambda: map(clean, db.get_group_list()), number=1000))
+    assert sorted(ui_list, key=Group.id_or_max) == sorted(db_list, key=Group.id_or_max)

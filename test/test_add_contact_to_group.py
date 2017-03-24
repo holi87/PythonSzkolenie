e9179186ae6e18abd0735baa6b2ab056e1358f98
holi87 <holi87@gmail.com>
2017-03-24 22:26:31 +0100

    working on task 22 - sprawdzenie czy sa jakies kontakty i ew dodanie przykladowego

--- test/test_add_contact_to_group.py
+++ test/test_add_contact_to_group.py
@@ -1,8 +1,14 @@
-
+from model.contact import Contact
+import random
 __author__ = "Grzegorz Holak"
 
 
 def test_add_contact_to_group(app, db, check_ui):
+    if len(db.get_contact_list()) == 0:
+        app.contact.create_without_photo(Contact(first_name="imieDodane", last_name="NazwiskoDodane"))
+
+    if check_ui:
+        pass
 
     """
     wejscie na strone glowna

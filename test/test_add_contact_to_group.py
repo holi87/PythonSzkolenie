88a2c328de2ae08f10dc70e2f6842d060c43bf73
holi87 <holi87@gmail.com>
2017-03-24 23:26:09 +0100

    working on task 22 - add contact to group is done (corrected)

--- test/test_add_contact_to_group.py
+++ test/test_add_contact_to_group.py
@@ -5,16 +5,21 @@ __author__ = "Grzegorz Holak"
 
 
 def test_add_contact_to_group(app, orm):
-    # checks if there is any contact and any group and making ones if there is none
-    if len(orm.get_contact_list()) == 0:
-        app.contact.create_without_photo(Contact(first_name="imieDodane", last_name="NazwiskoDodane"))
+    # checks if there is any group
     if len(orm.get_group_list()) == 0:
         app.group.create(Group(name="nazwa"))
     groups_list = orm.get_group_list()
     group = random.choice(groups_list)
+
+    # checks if there is any contact out of group (not matter if there is no contact at all or all are in group)
+    if len(orm.get_contacts_not_in_group(Group(group_id=group.group_id))) == 0:
+        app.contact.create_without_photo(Contact(first_name="imieDodane", last_name="NazwiskoDodane"))
+
     contacts_list = orm.get_contacts_not_in_group(Group(group_id=group.group_id))
     contact = random.choice(contacts_list)
+
     app.contact.select_contact_by_id(contact.contact_id)
     app.group.select_group_to_add_contact_by_id(group.group_id)
     app.contact.add_contact_to_group()
+    # checks in db if this contact was added to group
     assert contact in orm.get_contacts_in_group(Group(group_id=group.group_id))

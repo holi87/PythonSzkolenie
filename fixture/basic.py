bf0b6bb4d4f84aee0e5d93abbc763b7629db8064
holi87 <holi87@gmail.com>
2017-03-27 00:29:15 +0200

    maybe I just killed everything:D

--- fixture/basic.py
+++ fixture/basic.py
@@ -21,11 +21,11 @@ class BasicHelper:
         except:
             return False
 
-    def open_home_page(self):
-        wd = self.app.wd
-        if not (wd.current_url.endswith("/addressbook/") and wd.find_elements_by_xpath('.//*/select[@name="group"]')):
-            wd.get("http://localhost:8080/addressbook/")
-
+    # def open_home_page(self):
+    #     wd = self.app.wd
+    #     if not (wd.current_url.endswith("/addressbook/") and wd.find_elements_by_xpath('.//*/select[@name="group"]')):
+    #         wd.get("http://localhost:8080/addressbook/")
+    #
     def clear(self, s):
         return re.sub("[() -]", "", s)
 
